deef4ac22b20f9e7ec41d98fbe0645766446ebf4
Hadi Dbouk <hadiidbouk@outlook.com>
2019-06-11 17:29:40 +0300

    Present HeroesPickerViewController in popup mode

---
+++ MarvelAR/HeroesPickerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HeroesPickerViewController.swift
+//  MarvelAR
+//
+//  Created by Hadi Dbouk on 6/11/19.
+//  Copyright © 2019 hadiidbouk. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class HeroesPickerViewController: UIViewController {
+    
+    var sceneView: SCNView!
+    var size: CGSize!
+    
+    init(size: CGSize) {
+        super.init(nibName: nil, bundle: nil)
+        self.size = size
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.frame = CGRect(origin: .zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
+        
+        preferredContentSize = size
+    }
+}
--- MarvelAR/HeroesPlacerViewController.swift
+++ MarvelAR/HeroesPlacerViewController.swift
@@ -10,15 +10,12 @@ import UIKit
 import SceneKit
 import ARKit
 
-class HeroesPlacerViewController: UIViewController, ARSCNViewDelegate {
+class HeroesPlacerViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
@@ -46,30 +43,21 @@ class HeroesPlacerViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+    
+    @IBAction func onPickAHeroBtnPressed(_ sender: Any) {
+        let heroesPickerViewController = HeroesPickerViewController(size: CGSize(width: 250, height: 500))
+        heroesPickerViewController.modalPresentationStyle = .popover
+        heroesPickerViewController.popoverPresentationController?.delegate = self
+        present(heroesPickerViewController, animated: true, completion: nil)
+        heroesPickerViewController.popoverPresentationController?.sourceView = sender as? UIView
+    }
+}
 
-    // MARK: - ARSCNViewDelegate
+//MARK: UIPopoverPresentationControllerDelegate
+extension HeroesPlacerViewController : UIPopoverPresentationControllerDelegate {
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
