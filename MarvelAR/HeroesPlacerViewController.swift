a7031f7dd9f7d8e828f911bab491cd132bf3d90b
Hadi Dbouk <hadiidbouk@outlook.com>
2019-06-12 18:27:23 +0300

    Remove statistic bar

--- MarvelAR/HeroesPlacerViewController.swift
+++ MarvelAR/HeroesPlacerViewController.swift
@@ -16,15 +16,6 @@ class HeroesPlacerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        //let scene = SCNScene(named: "heroes.scnassets/captainAmerica/captainAmerica.scn")!
-        
-        // Set the scene to the view
-        //sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
